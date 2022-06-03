8dee97556b68a6a6ab43243b1988af20859a4b12
StVictoria <2018vvs@gmail.com>
2022-06-03 13:23:06 +0300

    added dots / arrows handler

--- src/components/testimonials/TestimonialsFooter.jsx
+++ src/components/testimonials/TestimonialsFooter.jsx
@@ -1,8 +1,29 @@
+import clsx from 'clsx';
+import { useState } from 'preact/hooks';
 import "./TestimonialsFooter.scss";
 
+const dots = [...Array(3).keys()];
+
 const TestimonialsFooter = () => {
-  const renderDots = () => [...Array(4).keys()].map((dot) => <div className="TestimonialsFooter-Dot" key={dot} />)
-  
+  const [activeDot, setActiveDot] = useState(0);
+
+  const handleClick = (direction) => {
+    if (direction === "right" && activeDot < dots.length - 1) {
+      setActiveDot(activeDot + 1);
+    }
+    if (direction === "left" && activeDot > dots[0]) {
+      setActiveDot((activeDot) => activeDot - 1);
+    }
+    return null;
+  };
+
+  const renderDots = () => dots.map((dot) => (
+    <button
+      className={clsx("TestimonialsFooter-Dot", { "TestimonialsFooter-Dot_active": activeDot === dot })}
+      key={dot}
+      onClick={() => { setActiveDot(dot); }} />
+  ));
+
   return (
     <footer className="TestimonialsFooter">
       <div className="TestimonialsFooter-Dots">
@@ -10,8 +31,16 @@ const TestimonialsFooter = () => {
         <div className="TestimonialsFooter-Slider" />
       </div>
       <div className="TestimonialsFooter-Buttons">
-        <button className="TestimonialsFooter-ArrowButton"><div className="TestimonialsFooter-ArrowLeft" /></button>
-        <button className="TestimonialsFooter-ArrowButton"><div className="TestimonialsFooter-ArrowRight" /></button>
+        <button
+          className="TestimonialsFooter-ArrowButton"
+          onClick={() => handleClick("left")}>
+          <div className="TestimonialsFooter-ArrowLeft" />
+        </button>
+        <button
+          className="TestimonialsFooter-ArrowButton"
+          onClick={() => handleClick("right")}>
+          <div className="TestimonialsFooter-ArrowRight" />
+        </button>
       </div>
     </footer>
   );
